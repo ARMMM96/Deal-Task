064f36bed12d70856713bd634bd84b674941854e
Ahmed Rabie <ahmed.r@trusstedge.com>
2024-05-23 09:06:01 +0300

    Route model implemented

---
+++ app/database/models/route.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const routeSchema = new mongoose.Schema({
+    url_name: {
+        type: String,
+        uniqe: true,
+        required: true,
+        trim: true,
+    },
+    roles: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Role",
+        },
+    ],
+});
+
+const Route = mongoose.model("Route", routeSchema);
+
+module.exports = Route;
