c0923d58c8f49d2ee1e78a325aee0a4ac29b6551
Ahmed Rabie <ahmed.r@trusstedge.com>
2024-05-23 19:18:03 +0300

    User model updated

--- app/database/models/user.model.js
+++ app/database/models/user.model.js
@@ -1,60 +1,75 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        trim: true,
-        lowercase: true,
-        minLength: 3,
-        maxLength: 50,
-        required: true,
-    },
-    phone: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        validate: {
-            validator: function (v) {
-                // Array of Arab country locales
-                const arabLocales = [
-                    "ar-AE", // United Arab Emirates
-                    "ar-BH", // Bahrain
-                    "ar-DZ", // Algeria
-                    "ar-EG", // Egypt
-                    "ar-IQ", // Iraq
-                    "ar-JO", // Jordan
-                    "ar-KW", // Kuwait
-                    "ar-LB", // Lebanon
-                    "ar-LY", // Libya
-                    "ar-MA", // Morocco
-                    "ar-OM", // Oman
-                    "ar-PS", // Palestine
-                    "ar-QA", // Qatar
-                    "ar-SA", // Saudi Arabia
-                    "ar-SD", // Sudan
-                    "ar-SY", // Syria
-                    "ar-TN", // Tunisia
-                    "ar-YE", // Yemen
-                ];
-                return validator.isMobilePhone(v, arabLocales);
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            lowercase: true,
+            minlength: [3, "Name must be at least 3 characters long"],
+            maxlength: [50, "Name must be less than 50 characters long"],
+            required: [true, "Name is required"],
+            validate: {
+                validator: function (v) {
+                    return validator.isAlpha(v.replace(/\s/g, ""), "en-US", {
+                        ignore: " ",
+                    });
+                },
+                message: (props) =>
+                    `${props.value} contains invalid characters`,
             },
-            message: (props) =>
-                `${props.value} is not a valid phone number in any of the specified Arab countries!`,
+        },
+        phone: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            validate: {
+                validator: function (v) {
+                    // Array of Arab country locales
+                    const arabLocales = [
+                        "ar-AE", // United Arab Emirates
+                        "ar-BH", // Bahrain
+                        "ar-DZ", // Algeria
+                        "ar-EG", // Egypt
+                        "ar-IQ", // Iraq
+                        "ar-JO", // Jordan
+                        "ar-KW", // Kuwait
+                        "ar-LB", // Lebanon
+                        "ar-LY", // Libya
+                        "ar-MA", // Morocco
+                        "ar-OM", // Oman
+                        "ar-PS", // Palestine
+                        "ar-QA", // Qatar
+                        "ar-SA", // Saudi Arabia
+                        "ar-SD", // Sudan
+                        "ar-SY", // Syria
+                        "ar-TN", // Tunisia
+                        "ar-YE", // Yemen
+                    ];
+                    return validator.isMobilePhone(v, arabLocales);
+                },
+                message: (props) =>
+                    `${props.value} is not a valid phone number in any of the specified Arab countries!`,
+            },
+        },
+        role: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Role",
+            required: [true, "Role is required"],
+        },
+        status: {
+            type: String,
+            enum: ["ACTIVE", "DELETED"],
+            default: "ACTIVE",
         },
     },
-    role: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Role",
-        required: true,
-    },
-    status: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Status",
-        required: true,
-    },
-});
+    {
+        collection: "users",
+        timestamps: true,
+    }
+);
 
 const User = mongoose.model("User", userSchema);
 
