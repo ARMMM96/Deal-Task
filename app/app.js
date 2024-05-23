90cfc00bd37d7320f7ae78970e1cdc1c8d58118a
Ahmed Rabie <ahmed.r@trusstedge.com>
2024-05-23 08:29:08 +0300

    Editorconfig implemented
        .editorconfig file will help with
        standardizing code such as
        indentation and quotes

--- app/app.js
+++ app/app.js
@@ -1,12 +1,11 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
 const app = express();
-const connectDB = require('./database/connection')
-
+const connectDB = require("./database/connection");
 
 app.use(cors());
 
-connectDB()
+connectDB();
 
 module.exports = app;
