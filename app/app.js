11a6c6b84f7158b892df97c746a8eb0b18e40cfb
Ahmed Rabie <ahmed.r@trusstedge.com>
2024-05-23 07:20:12 +0300

    "Database connection established"

--- app/app.js
+++ app/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const app = express();
+const connectDB = require('./database/connection')
+
 
 app.use(cors());
 
+connectDB()
 
 module.exports = app;
