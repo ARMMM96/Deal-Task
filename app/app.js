b05ac83601378949550345b845eb0b3d4eea9ebb
Ahmed Rabie <ahmed.r@trusstedge.com>
2024-05-25 09:25:36 +0300

    App js updated

--- app/app.js
+++ app/app.js
@@ -3,9 +3,14 @@ const path = require("path");
 const cors = require("cors");
 const app = express();
 const connectDB = require("./database/connection");
+const userRoutes = require('./routes/user.routes');
 
 app.use(cors());
-
+app.use(express.json())
 connectDB();
+
+// Routes
+app.use('/api/users', userRoutes);
+
 
 module.exports = app;
