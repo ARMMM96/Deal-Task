3dc59c70191859767e5cc0e6afb0fb028951766a
Ahmed Rabie <ahmed.r@trusstedge.com>
2024-05-25 11:25:01 +0300

    App updated     Roles routes linked to app.js

--- app/app.js
+++ app/app.js
@@ -3,16 +3,17 @@ const path = require("path");
 const cors = require("cors");
 const app = express();
 const connectDB = require("./database/connection");
-const userRoutes = require('./routes/user.routes');
-const route = require('./routes/route.routes');
+const userRoutes = require("./routes/user.routes");
+const route = require("./routes/route.routes");
+const roleRoutes = require("./routes/role.routes");
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 connectDB();
 
 // Routes
-app.use('/api/users', userRoutes);
-app.use('/api/routes', route);
-
+app.use("/api/users", userRoutes);
+app.use("/api/routes", route);
+app.use("/api/roles", roleRoutes);
 
 module.exports = app;
