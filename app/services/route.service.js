45ed38e2c64a610a58ad0d6d543094002fe06b3a
Ahmed Rabie <ahmed.r@trusstedge.com>
2024-05-25 20:51:46 +0300

    Route service updated

--- app/services/route.service.js
+++ app/services/route.service.js
@@ -1,4 +1,4 @@
-const Route = require('../database/models/route.model');
+const Route = require("../database/models/route.model");
 
 class RouteService {
     static async createRoute(routeData) {
@@ -14,11 +14,34 @@ class RouteService {
     }
 
     static async updateRoute(routeId, updateData) {
-        return await Route.findByIdAndUpdate(routeId, updateData, { new: true });
+        return await Route.findByIdAndUpdate(routeId, updateData, {
+            new: true,
+        });
     }
 
     static async deleteRoute(routeId) {
         return await Route.findByIdAndDelete(routeId);
+    }
+    static async findRouteByName(url_name) {
+        return await Route.findOne({ url_name });
+    }
+
+    static async createRoute(url_name) {
+        return await Route.create({ url_name, roles: [] });
+    }
+
+    static async saveRoutesToDatabase(routes) {
+        for (const route of routes) {
+            try {
+                const url = Object.keys(route.methods)[0] + " " + route.path;
+                const existingRoute = await this.findRouteByName(url);
+                if (!existingRoute) {
+                    await this.createRoute(url);
+                }
+            } catch (error) {
+                console.error(`Failed to save route ${route.path}:`, error);
+            }
+        }
     }
 }
 
